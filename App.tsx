98abb1681c1da4df1db1dd271d6960aa5c812058
xiaofanliu77 <62279464+xiaofanliu77@users.noreply.github.com>
2023-06-09 22:28:20 -0700

    use flatlist for displaying action cards and add images

--- App.tsx
+++ App.tsx
@@ -20,6 +20,7 @@ import {
   Button,
   Alert,
   FlatList,
+  Image,
 } from 'react-native';
 
 import {
@@ -57,6 +58,25 @@ const TrainingCard = ({title, description}: CardProps) => (
   </TouchableOpacity>
 );
 
+function ActionCard({exercises}: any): JSX.Element {
+  // console.log(exercises[0]);
+  return (
+    <FlatList
+      data={exercises}
+      renderItem={({item}) => (
+        <View style={styles.actionCard}>
+          {/* <Text>{item.iconImage}</Text> */}
+          <Image style={styles.cardImage} source={require('./dumbell.png')} />
+          <Text style={styles.actionCardText}>{item.name}</Text>
+        </View>
+      )}
+      numColumns={2}
+      horizontal={false}
+    />
+  );
+}
+
+// ExerciseList is replaced by ActionCard
 function ExerciseList({exercises}: any): JSX.Element {
   return exercises.map((ex, index) => {
     return (
@@ -83,7 +103,8 @@ function EquipmentList({value}: any): JSX.Element {
           {`    `}
           {key}
         </Text>
-        <ExerciseList exercises={val} />
+        {/* <ExerciseList exercises={val} /> */}
+        <ActionCard exercises={val} />
       </View>
     );
   });
@@ -104,7 +125,7 @@ function SubBodyPartList({value}: any): JSX.Element {
   });
 }
 
-function BodyPartList(): JSX.Element {
+function BodyPartList({value}: any): JSX.Element {
   return _.map(actionItems, (val: any[], key: any) => {
     // console.log({k: key})
     return (
@@ -114,9 +135,9 @@ function BodyPartList(): JSX.Element {
       </View>
     );
   });
-}
 
-console.log('A');
+  // return <FlatList data={value} renderItem={} />;
+}
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -127,21 +148,26 @@ function App(): JSX.Element {
   // console.log('B')
   return (
     /////////// USING FLATLIST ///////////////////////////
-    // <SafeAreaView style={styles.container}>
-    //   <StatusBar
-    //     barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-    //     backgroundColor={backgroundStyle.backgroundColor}
-    //   />
-    //   <Header />
-    //   <FlatList
-    //     data={data}
-    //     renderItem={({item}) => <TrainingCard title={item.title} description={item.description}/>}
-    //     numColumns={2}
-    //     contentContainerStyle={styles.container}
-    //   />
-    // </SafeAreaView>
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      {/* <Header /> */}
+      {/* <FlatList
+        data={data}
+        renderItem={({item}) => (
+          <TrainingCard title={item.title} description={item.description} />
+        )}
+        numColumns={2}
+        contentContainerStyle={styles.container}
+      /> */}
+
+      <BodyPartList />
+    </SafeAreaView>
 
     ///////////// USING Flexbox //////////////
+    /*
     <ScrollView>
       <Header />
       <View style={styles.container}>
@@ -156,25 +182,30 @@ function App(): JSX.Element {
       </View>
       <BodyPartList />
     </ScrollView>
+    */
   );
 }
 
 const cardWidth = screenDimensions.width / 2 - 20;
 
-console.log('C');
-
 const styles = StyleSheet.create({
+  //Flexbox
+  /*
   container: {
     backgroundColor: '#f0f0f0',
     flexDirection: 'row',
     flexWrap: 'wrap',
-    justifyContent: 'space-evenly',
-    alignItems: 'flex-start',
-    alignContent: 'flex-start',
+
+    // justifyContent: 'space-evenly',
+    // padding: 5,
+    // alignItems: 'flex-start',
+    // alignContent: 'flex-start',
     // flex: 1
   },
+  */
   trainingCard: {
     marginVertical: 8,
+    marginLeft: (screenDimensions.width - cardWidth * 2) / 3,
     width: cardWidth,
     height: 100,
     borderRadius: 10,
@@ -183,15 +214,44 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     justifyContent: 'space-between',
   },
+
+  //FlatList
+  container: {
+    backgroundColor: '#f0f0f0',
+    flex: 1,
+  },
+  actionCard: {
+    marginVertical: 8,
+    marginLeft: (screenDimensions.width - cardWidth * 2) / 3,
+    width: cardWidth * 0.8,
+    height: 120,
+    borderRadius: 10,
+    backgroundColor: '#fbfbfb',
+    padding: 10,
+    // flexDirection: 'row',
+    // flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   cardTitle: {
     fontSize: 14,
     margin: 10,
     fontWeight: 'bold',
-    flexBasis: '50%',
+    flexBasis: '50%', //ensure first row of card contains only 2 items
   },
   cardText: {
     fontSize: 12,
     margin: 10,
+  },
+  cardImage: {
+    margin: 10,
+    flex: 1,
+    width: cardWidth * 0.8,
+    height: cardWidth * 0.8 - 20,
+    resizeMode: 'contain',
+  },
+  actionCardText: {
+    // textAlign: 'center',
   },
 });
 
